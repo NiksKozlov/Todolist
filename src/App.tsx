819956fb44d1bb3e98ab8b9a5eb1910863078483
NiksKozlov <nikskozlov@gmail.com>
2022-08-16 02:43:01 +0300

    adding props and a new todolist

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+    const task1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false}
+    ]
+    const task2 = [
+        {id: 1, title: 'Hello World', isDone: true},
+        {id: 2, title: 'I am Happy', isDone: false},
+        {id: 3, title: 'Yo', isDone: false}
+    ]
+
     return (
         <div className="App">
-            <Todolist/>
-            <Todolist/>
-            <Todolist/>
+            <Todolist title="What to learn" tasks={task1}/>
+            <Todolist title="Songs" tasks={task2}/>
         </div>
     )
 }
