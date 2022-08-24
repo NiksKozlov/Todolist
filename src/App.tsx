85fd4f1962ff0b003c9438a118c1c450ee8ea04d
NiksKozlov <nikskozlov@gmail.com>
2022-08-25 01:15:29 +0300

    add filtering to button 'all', 'active' and 'completed'

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,47 @@ import React, {useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from './Todolist';
 
+export type FilterValuesType = 'all' | 'active' | 'completed'
+
 function App() {
     //BLL:
+    const todoListTitle: string = "What to learn"
+
     const [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false}
     ])
+    const [filter, setFilter] = useState<FilterValuesType>('all')
+
     const removeTask = (taskID: number) => {
         setTasks(tasks.filter(t => t.id !== taskID))
     }
 
+    const changeFilter = (filter: FilterValuesType) => {
+        setFilter(filter)
+    }
+
+    const getTasksForTodolist = () => {
+        switch (filter) {
+            case "active":
+                return tasks.filter(t => !t.isDone)
+            case "completed":
+                return tasks.filter(t => t.isDone)
+            default:
+                return tasks
+        }
+    }
 
     //UI:
     return (
         <div className="App">
             <Todolist
-                title="What to learn"
-                tasks={tasks}
-                removeTask={removeTask} />
+                title={todoListTitle}
+                tasks={getTasksForTodolist()}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
         </div>
     );
 }
